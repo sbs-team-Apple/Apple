a948a3ed7589c15a796903562709ad941a80b494
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-10-06 12:29:22 +0900

    setting

---
+++ src/test/java/com/sbs/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
