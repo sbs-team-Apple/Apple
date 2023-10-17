f628b835604ea5c27c85a3da3648eab4b302528a
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-10-17 10:53:33 +0900

    테스트 더미

--- src/main/java/com/sbs/apple/MainController.java
+++ src/main/java/com/sbs/apple/MainController.java
@@ -3,7 +3,9 @@ package com.sbs.apple;
 import com.sbs.apple.chat.ChatRoomService;
 import com.sbs.apple.user.SiteUser;
 import com.sbs.apple.user.UserService;
+import com.sbs.apple.util.DataCreator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,15 @@ public class MainController {
 
     private final ChatRoomService chatRoomService;
     private final UserService userService;
+
+    private final DataCreator dataCreator;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAutoValue;
+
+    // ************ TEST DATA CREATE ***************
+    private boolean isTestDataCreated = false;
+    // *********************************************
+
     @GetMapping("/")
     public String showMain(Model model) {
         //ChatRoom chatRoom= chatRoomService.findLastRoom();
@@ -24,6 +35,13 @@ public class MainController {
         //}
 
         //model.addAttribute("chatRoom", chatRoom);
+
+        // ************ TEST DATA CREATE ***************
+        if (!isTestDataCreated && ddlAutoValue.equals("create")) {
+            dataCreator.createTestData();
+            isTestDataCreated = true;
+        }// *********************************************
+
         List<SiteUser> userList = userService.getFourUsers(); // 사용자 정보를 가져오는 예시 메서드
         model.addAttribute("userList", userList);
         return "main";
