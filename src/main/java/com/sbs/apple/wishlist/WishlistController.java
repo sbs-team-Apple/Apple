65411d1d5824dd3f148078ebd238fe7e61aa8332
kimaruy <changmin2458@gmail.com>
2023-10-25 11:42:51 +0900

    Aa

--- src/main/java/com/sbs/apple/wishlist/WishlistController.java
+++ src/main/java/com/sbs/apple/wishlist/WishlistController.java
@@ -2,15 +2,20 @@ package com.sbs.apple.whislist;
 
 import com.sbs.apple.user.SiteUser;
 import com.sbs.apple.user.UserService;
+import com.sbs.apple.wishlist.WishlistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 @RequiredArgsConstructor
 @Controller
@@ -18,14 +23,19 @@ import java.util.Map;
 public class WishlistController {
     @Autowired
     private UserService userService;
+    @Autowired
     private WishlistRepository wishlistRepository;
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/add")
     public String addToWishlist(Model model, Principal principal) {
         String username = principal.getName();
-        SiteUser user = userService.getUserbyName(username);
-        model.addAttribute("user", user);
+        SiteUser ownerUser = userService.getUserbyName(username);
+
+        if (ownerUser != null) {
+            List<SiteUser> addedUsers = wishlistRepository.findByOwner(ownerUser.getId().toString());
+            model.addAttribute("addedUsers", addedUsers);
+        }
 
         return "add";
     }
@@ -36,16 +46,22 @@ public class WishlistController {
     public ResponseEntity<String> addToWishlist(@RequestBody Map<String, String> requestData, Principal principal) {
         String addedUserId = requestData.get("userId");
         String username = principal.getName();
-        SiteUser ownerUser = userService.getUserbyName(username);
-        SiteUser addedUser = userService.getUserbyId(addedUserId);
 
-        if (ownerUser != null && addedUser != null) {
+        System.out.println("addedUserId : " + addedUserId);
+        System.out.println("username : " + username);
+
+        SiteUser ownerUser = userService.getUserbyName(username);
+//        SiteUser addedUser = userService.getUserbyId(addedUserId);
+
+        if (ownerUser != null) {
             SiteUser siteUser = new SiteUser();
-            siteUser.setOwner(ownerUser.toString()); // ownerUser를 문자열로 변환하여 할당
-            siteUser.setAddedUser(addedUser.toString()); // addedUser를 문자열로 변환하여 할당
+            siteUser.setOwner(ownerUser.getId().toString());
+            // ownerUser를 문자열로 변환하여 할당
+//            siteUser.setAddedUser(addedUser.toString()); // addedUser를 문자열로 변환하여 할당
 
             // 데이터베이스에 저장
             wishlistRepository.save(siteUser);
+            System.out.println("siteUser__ : " + siteUser);
 
             return ResponseEntity.ok("사용자가 찜 목록에 추가되었습니다.");
         } else {
@@ -54,4 +70,3 @@ public class WishlistController {
     }
 
 }
-
