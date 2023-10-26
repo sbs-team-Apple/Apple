c2856ad2214b7645bd9a2485ccf5f7f0e599cf50
kimaruy <changmin2458@gmail.com>
2023-10-26 10:46:19 +0900

    aa

--- src/main/java/com/sbs/apple/interest/InterestRepository.java
+++ src/main/java/com/sbs/apple/interest/InterestRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InterestRepository extends JpaRepository<Interest, Integer> {
-    List<Interest> findAllByInterest_user(String username);
+    List<Interest> findAllByInterestUser(String username);
 }
