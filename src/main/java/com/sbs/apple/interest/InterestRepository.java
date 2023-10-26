9dc3e3740f451e58b368dd2078509b8feb79b167
seongghun <esd2727@gmail.com>
2023-10-26 10:45:00 +0900

    qweQWQWE

--- src/main/java/com/sbs/apple/interest/InterestRepository.java
+++ src/main/java/com/sbs/apple/interest/InterestRepository.java
@@ -2,5 +2,9 @@ package com.sbs.apple.interest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterestRepository extends JpaRepository<Interest, Integer> {
+
+    List<Interest> findAllByInterestUser(String interestUser);
 }
