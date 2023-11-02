052fe2da761a41d38740ae518ca596a50d3c888d
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-11-02 19:18:09 +0900

    dsfg

--- src/main/java/com/sbs/apple/MainController.java
+++ src/main/java/com/sbs/apple/MainController.java
@@ -5,6 +5,7 @@ import com.sbs.apple.chat.ChatRoomService;
 import com.sbs.apple.notification.Notification;
 import com.sbs.apple.notification.NotificationService;
 import com.sbs.apple.user.SiteUser;
+import com.sbs.apple.user.UserRepository;
 import com.sbs.apple.user.UserService;
 import com.sbs.apple.util.DataCreator;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,7 @@ public class MainController {
     private final UserService userService;
     private final DataCreator dataCreator;
     private final NotificationService notificationService;
+    private final UserRepository userRepository;
 
 
     @Value("${spring.jpa.hibernate.ddl-auto}")
@@ -32,13 +34,14 @@ public class MainController {
     // ************ TEST DATA CREATE ***************
     private boolean isTestDataCreated = false;
     // *********************************************
+    long recordCount = userRepository.count();
 
     @GetMapping("/")
     public String showMain(Model model, Principal principal) {
         System.out.println("메인페이지 실행1");
 
         // ************ TEST DATA CREATE ***************
-        if (!isTestDataCreated && ddlAutoValue.equals("create")) {
+        if (!isTestDataCreated && ddlAutoValue.equals("create")&& recordCount == 0) {
             dataCreator.createTestData();
             isTestDataCreated = true;
 
