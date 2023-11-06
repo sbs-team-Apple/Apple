095e3d7949eb5136625a9178efb5d1727de576c0
kimaruy <changmin2458@gmail.com>
2023-11-06 10:46:50 +0900

    AA

--- src/main/java/com/sbs/apple/user/UserController.java
+++ src/main/java/com/sbs/apple/user/UserController.java
@@ -6,7 +6,8 @@ import com.sbs.apple.chat.ChatRoom;
 import com.sbs.apple.chat.ChatRoomService;
 import com.sbs.apple.cybermoney.CyberMoneyTransaction;
 import com.sbs.apple.cybermoney.CyberMoneyTransactionRepository;
-import com.sbs.apple.interest.Interest;
+import com.sbs.apple.exchange.ExchangeRepository;
+import com.sbs.apple.exchange.ExchangeService;
 import com.sbs.apple.interest.InterestService;
 import com.sbs.apple.report.ReportForm;
 import com.sbs.apple.report.ReportService;
@@ -47,6 +48,8 @@ public class UserController {
     private final InterestService interestService;
     private final CyberMoneyTransactionRepository cyberMoneyTransactionRepository;
     private final ChatRoomService chatRoomService;
+    private final ExchangeService exchangeService;
+    private final ExchangeRepository exchangeRepository;
 
 
     @GetMapping("/signup")
@@ -273,15 +276,6 @@ public class UserController {
         return "redirect:/user/myPage";
     }
 
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/payment")
-    public String paymentPage(Model model, Principal principal) {
-        String username = principal.getName();
-        SiteUser user = userService.getUserbyName(username);
-        model.addAttribute("user", user);
-        return "/pay/payment";
-    }
-
     //조회하기
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/detail/{id}")
@@ -292,13 +286,13 @@ public class UserController {
         boolean isInterested = interestService.isInterested(id, interest_user);
         model.addAttribute("isInterested", isInterested);
 
-        SiteUser loginUser= userService.getUserbyName(principal.getName());
+        SiteUser loginUser = userService.getUserbyName(principal.getName());
 
         //로그인한 사용자와 현재 프로필 선택한 유저 사이의 채팅방을 찾는 코드
-        ChatRoom chatRoom=chatRoomService.findRoomByUserIdAndUserId2(siteUser.getId(), loginUser.getId());
+        ChatRoom chatRoom = chatRoomService.findRoomByUserIdAndUserId2(siteUser.getId(), loginUser.getId());
 
         //현재 그유저와 채팅방이 있으면 채팅방 만들기 버튼은 아예 안만들 생각
-        model.addAttribute("chatRoom",chatRoom);
+        model.addAttribute("chatRoom", chatRoom);
 
 
         return "user/profile";
@@ -364,61 +358,6 @@ public class UserController {
         SiteUser user = userService.getUserbyName(principal.getName());
         userService.photoModify(user, boardForm.getFile());
         return "redirect:/user/myPage";
-    }
-
-    //관심 추가하기
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/add_interest/{id}")
-    public String toggleInterest(Principal principal, @PathVariable Integer id, Model model) {
-        String interest_user = principal.getName();
-        model.addAttribute("userId", id);
-        boolean isInterested = interestService.isInterested(id, interest_user);
-        if (isInterested) {
-            interestService.removeInterest(id, interest_user);
-        } else {
-            interestService.addInterest(id, interest_user);
-        }
-        return "redirect:/user/detail/{id}";
-    }
-
-
-    //내가 관심있는 사람 조회하기
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/wish")
-    public String showWish(Principal principal, Model model) {
-        String username = principal.getName();
-        List<Interest> interestList = interestService.getWishUsers(username);
-        model.addAttribute("interestList", interestList);
-        return "wish";
-    }
-
-    //나에게 관심 있는 사람 조회하기
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/wished")
-    public String showWished(Principal principal, Model model) {
-        String username = principal.getName();
-        List<Interest> interestList = interestService.getWishedUsers(username);
-        model.addAttribute("interestList", interestList);
-        return "wished";
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/exchange")
-    public String exchange(Model model, Principal principal) {
-        String username = principal.getName();
-        SiteUser user = userService.getUserbyName(username);
-        model.addAttribute("user", user);
-
-        return "pay/exchange";
-    }
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/exchange_apply")
-    public String exchange_apply(Model model, Principal principal) {
-        String username = principal.getName();
-        SiteUser siteUser = userService.getUserbyName(username);
-        model.addAttribute("siteUser", siteUser);
-
-        return "pay/exchange_apply";
     }
 
     @GetMapping("/transactions")
@@ -509,18 +448,18 @@ public class UserController {
 
         //채팅방 만드는건 여기서부터 구현
 
-        ChatRoom chatRoom= chatRoomService.findLastRoom();
+        ChatRoom chatRoom = chatRoomService.findLastRoom();
         //만들어진 채팅 방이 없을시 기본값 채팅방 번호 1 부여
-        int roomId=1;
+        int roomId = 1;
 
-        if(chatRoom != null){
-        roomId=chatRoom.getId()+1;}
+        if (chatRoom != null) {
+            roomId = chatRoom.getId() + 1;
+        }
 
         //사이버 머니 받은 사용자의 id 즉 채팅방 초대받는 유저 인덱스번호
 
 
-
-        return "redirect:/chat/"+roomId+"/room/"+toUserId; // 거래 내역 페이지로 리다이렉트
+        return "redirect:/chat/" + roomId + "/room/" + toUserId; // 거래 내역 페이지로 리다이렉트
     }
 
 }
