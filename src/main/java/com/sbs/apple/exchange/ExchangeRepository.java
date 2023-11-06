1c0f85cf1cf03c406d708432bdfcbc1552ba4d93
kimaruy <changmin2458@gmail.com>
2023-11-06 10:46:50 +0900

    AA

---
+++ src/main/java/com/sbs/apple/exchange/ExchangeRepository.java
@@ -0,0 +1,6 @@
+package com.sbs.apple.exchange;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRepository  extends JpaRepository<Exchange, Integer> {
+}
