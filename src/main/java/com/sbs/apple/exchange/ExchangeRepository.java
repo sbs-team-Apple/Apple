5d9b19949a98fe37d9effe6f070adb89951c68ee
kimaruy <changmin2458@gmail.com>
2023-11-07 13:35:26 +0900

    AA

--- src/main/java/com/sbs/apple/exchange/ExchangeRepository.java
+++ src/main/java/com/sbs/apple/exchange/ExchangeRepository.java
@@ -1,6 +1,11 @@
 package com.sbs.apple.exchange;
 
+import com.sbs.apple.user.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExchangeRepository  extends JpaRepository<Exchange, Integer> {
+import java.util.Optional;
+
+public interface ExchangeRepository extends JpaRepository<Exchange, Integer> {
+
+    Optional<Exchange> findBySiteUser(SiteUser siteUser);
 }
