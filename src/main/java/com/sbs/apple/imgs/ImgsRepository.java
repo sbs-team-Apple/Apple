d8daabc6c9f515994ba7a09faf3c5d74c603db52
guenguen97 <guenguen97@gmail.com>
2023-11-15 11:53:18 +0900

    사진 삭제는 되는데 사진 하나만 남기면 아예 사진이 다사젝됨

--- src/main/java/com/sbs/apple/imgs/ImgsRepository.java
+++ src/main/java/com/sbs/apple/imgs/ImgsRepository.java
@@ -2,8 +2,10 @@ package com.sbs.apple.imgs;
 
 import com.sbs.apple.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,4 +19,8 @@ public interface ImgsRepository extends JpaRepository<Imgs, Integer> {
   @Query("SELECT i FROM Imgs i WHERE i.board = :board AND i.indexA = :index")
   Imgs findByBoardIdAndIndexA(@Param("board") Board board, @Param("index") Integer index);
 
+  @Transactional
+  @Modifying
+  @Query("DELETE FROM Imgs i WHERE i.board = :board AND i.indexA IN :deleteIndex")
+  void deleteByBoardAndIndexA(@Param("board")Board board,@Param("deleteIndex") List<Integer> deleteIndex);
 }
