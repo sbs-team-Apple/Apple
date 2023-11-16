d160db1ec41c2231b6d65cd3c346cf7615dd73eb
kimaruy <changmin2458@gmail.com>
2023-11-16 13:49:58 +0900

    WW

--- src/main/java/com/sbs/apple/AppleApplication.java
+++ src/main/java/com/sbs/apple/AppleApplication.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.IOException;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class AppleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppleApplication.class, args);
+
+		AccessTokenRequest tokenRequest = new AccessTokenRequest();
+
+		try {
+			String tokenResponse = tokenRequest.requestToken();
+			System.out.println("Token Response: " + tokenResponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
