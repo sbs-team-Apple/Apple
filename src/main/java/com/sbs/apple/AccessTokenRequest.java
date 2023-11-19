20262c1ab65f28fcfdd97b552edd9ba7b68f2f8d
kimaruy <changmin2458@gmail.com>
2023-11-20 07:08:16 +0900

    HH

--- src/main/java/com/sbs/apple/AccessTokenRequest.java
+++ src/main/java/com/sbs/apple/AccessTokenRequest.java
@@ -1,51 +1,41 @@
 package com.sbs.apple;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+
 
 public class AccessTokenRequest {
 
-    public String requestToken() throws IOException {
-        String clientId = "445795d2-e19e-4da7-af8a-7634a6e68e67";
+    public String requestToken() throws Exception {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
+        String clientID = "445795d2-e19e-4da7-af8a-7634a6e68e67";
         String clientSecret = "3be8754bbb31422e1a5f7b8c97f7f7cc";
-        String credentials = clientId + ":" + clientSecret;
+        String credentials = clientID + ":" + clientSecret;
+        String base64Credentials = java.util.Base64.getEncoder().encodeToString(credentials.getBytes());
 
-        String base64Credentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+        HttpPost request = new HttpPost("http://svc.niceapi.co.kr:22001/digital/niceid/oauth/oauth/token");
+        request.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        request.addHeader("Authorization", "Basic " + base64Credentials);
 
-        String apiUrl = "http://svc.niceapi.co.kr:22001/digital/niceid/oauth/oauth/token";
-        String postData = "grant_type=client_credentials&scope=default";
+        StringEntity params = new StringEntity("grant_type=client_credentials&scope=default");
+        request.setEntity(params);
 
-        URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-        connection.setRequestProperty("Authorization", "Basic " + base64Credentials);
-        connection.setDoOutput(true);
+        HttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
 
-        connection.getOutputStream().write(postData.getBytes(StandardCharsets.UTF_8));
-
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            in.close();
-            return response.toString();
-        } else {
-            return "Token Request Failed. Response Code: " + responseCode;
+        if (entity != null) {
+            String responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+            return responseString;
         }
+
+        return null;
     }
 }
-
-
-
