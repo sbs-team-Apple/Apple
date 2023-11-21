b3be585d74425ad0b65edb878f707961cae0faab
guenguen97 <guenguen97@gmail.com>
2023-11-21 12:13:55 +0900

    알림 목록에서 x 누르면아작스로 바로바로 db 에서 기록 삭제됨

--- src/main/java/com/sbs/apple/notification/NotificationController.java
+++ src/main/java/com/sbs/apple/notification/NotificationController.java
@@ -3,11 +3,13 @@ package com.sbs.apple.notification;
 
 import com.sbs.apple.user.SiteUser;
 import com.sbs.apple.user.UserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -18,6 +20,10 @@ import java.util.List;
 public class NotificationController {
     private final NotificationService notificationService;
     private  final UserService userService;
+
+    public record YourRequestBodyClass(Integer notificationId){
+
+    }
 
 
     //아작스에서 쓰이는 함수
@@ -38,4 +44,22 @@ public class NotificationController {
 
         return notificationDTOs;
     }
+
+
+    @PostMapping("/delete/notification")
+    @ResponseBody
+    public String deleteNotification(@RequestBody YourRequestBodyClass requestBody){
+        Integer notificationId = requestBody.notificationId();
+
+        System.out.println(notificationId+"  ㅇㄴㅁ 알림 아이디!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Notification notification=notificationService.findById(notificationId);
+                notificationService.delete(notification);
+
+        System.out.println("알림 삭제함수실행됨!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        return "성공";
+    }
+
+
+
+
 }
