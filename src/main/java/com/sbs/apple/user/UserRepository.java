b6b88b9c1c8407883771a892ef977c5fc01c3496
seongghun <esd2727@gmail.com>
2023-11-27 02:21:14 +0900

    sdfs

--- src/main/java/com/sbs/apple/user/UserRepository.java
+++ src/main/java/com/sbs/apple/user/UserRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
     Optional<SiteUser> findByUsername(String username);
     Optional<SiteUser> findByNickname(String nickname);
-
+    Optional<SiteUser> findByEmail(String email);
     boolean existsByUsername(String username);
 
 
@@ -22,6 +22,7 @@ public interface UserRepository extends JpaRepository<SiteUser, Integer> {
 
     @Query("SELECT su FROM SiteUser su WHERE  su.gender <> :userGender AND (su.living = :desiredLiving OR su.religion = :desiredReligion)")
     List<SiteUser> findByDesired( @Param("userGender") String userGender, @Param("desiredLiving") String desiredLiving, @Param("desiredReligion") String desiredReligion);
+
 
 
 }
