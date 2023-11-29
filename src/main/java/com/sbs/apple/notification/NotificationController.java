9b9cdbecc93a0fcce6aae3e66bc63be47208410c
guenguen97 <guenguen97@gmail.com>
2023-11-29 13:39:32 +0900

    ì¢그냥 하트 보낼떄도 ãã알림 ã뜨게함

--- src/main/java/com/sbs/apple/notification/NotificationController.java
+++ src/main/java/com/sbs/apple/notification/NotificationController.java
@@ -29,20 +29,19 @@ public class NotificationController {
     //아작스에서 쓰이는 함수
     @GetMapping("/getNewNotifications")
     @ResponseBody
-    public List<NotificationDTO> getNewNotifications(Principal principal, Model model) {
+    public NotificationDTO getNewNotifications(Principal principal, Model model) {
         // 여기에서 실제로 새로운 알림 정보를 가져오는 로직을 구현
         // 반환된 데이터는 클라이언트로 전송됨
         SiteUser loginUser = userService.getUserbyName(principal.getName());
-        List<Notification> newNotifications = notificationService.getByUserTo(loginUser);
+        Notification newNotification = notificationService.getByUserTo(loginUser);
 
         List<NotificationDTO> notificationDTOs = new ArrayList<>();
-        for (Notification notification : newNotifications) {
-            NotificationDTO dto = new NotificationDTO(notification);
+            NotificationDTO dto = new NotificationDTO(newNotification);
             notificationDTOs.add(dto);
-        }
 
 
-        return notificationDTOs;
+
+        return no;
     }
 
 
